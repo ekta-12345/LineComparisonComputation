c82a6da5758074730ac744f1c545d17beaff9afb
ekta12345 <ektakumbhare3@gmail.com>
2021-04-09 12:18:58 +0530

    Added code for calculating length of line

--- Program.cs
+++ Program.cs
@@ -2,12 +2,27 @@
 
 namespace LineComparisonComputation
 {
-    class DisplayMessage
+    class ModelCartesian
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Computation Program");
-            Console.Read();
+            //taking input from user
+            Console.WriteLine("Enter the value of x1:");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of x2:");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of y1:");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of y2:");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            double length = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            Console.WriteLine("Length of (x, y)co-ordinates : " + length);
+
+
         }
     }
 }
