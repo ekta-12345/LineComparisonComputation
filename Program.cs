833080975421299c595bff65bee51b9db216ebe3
ekta12345 <ektakumbhare3@gmail.com>
2021-04-09 10:57:46 +0530

    Displaying message for computation of Line comparison program

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LineComparisonComputation
+{
+    class DisplayMessage
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            Console.Read();
+        }
+    }
+}
